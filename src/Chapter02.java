6d78a31b56454c1070fd162e5238286748df2e62
urotansh <114655455+urotansh@users.noreply.github.com>
2023-01-24 10:38:54 +0900

    Change indentation from tabs to spaces

--- src/Chapter02.java
+++ src/Chapter02.java
@@ -1,6 +1,6 @@
 
 public class Chapter02 {
-	public static void main(String[] args) {
-		System.out.println("こんにちは！");
-	}
+    public static void main(String[] args) {
+        System.out.println("こんにちは！");
+    }
 }
